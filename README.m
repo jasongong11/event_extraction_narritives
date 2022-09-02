a21de7be61fd12b5910cb05a53266891125e12b9
Jason Gong <xuanjungong@gmail.com>
2022-09-01 23:12:49 -0700

    initial commit

---
+++ README.m
@@ -0,0 +1,4 @@
+This project is a preliminary exploration of event extraction of narritives.
+The utimate goal is to analyze the structure of narritives and compare different types of narritives based on their structure and effects.
+
+Author: Xuanjun Gong
